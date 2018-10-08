6072b9afbd6d545d52b63095290dc1011d45157f
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2018-10-08 19:08:51 +0100

    Read k8s config (had to rerun godep save ./...)
    Tries to read k8s config from in-cluster and if that
    fails tries to load config from ~/.kube/config using
    current context

--- config.go
+++ config.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 // NOTE: There is a nice package which uses tags to load configuration from
@@ -20,32 +23,51 @@ type Config struct {
 }
 
 func LoadConfig() *Config {
-	config := Config{
-		Logger: NewLogger("unidler"),
+	logger := NewLogger("unidler")
+
+	k8sconfig, err := getK8sConfig()
+	if err != nil {
+		logger.Panicf("Couldn't get kubernetes config: %s", err)
 	}
 
-	k8sconfig, err := rest.InClusterConfig()
-	if err != nil {
-		panic(err.Error())
-	}
 	k8s, err := kubernetes.NewForConfig(k8sconfig)
 	if err != nil {
-		panic(err.Error())
+		logger.Panicf("Couldn't create kubernetes client: %s", err)
 	}
-
-	config.K8s = k8s
 
 	port, ok := os.LookupEnv("PORT")
 	if !ok {
 		port = ":8080"
 	}
-	config.Port = port
 
 	ingress_class_name, ok := os.LookupEnv("INGRESS_CLASS_NAME")
 	if !ok {
 		ingress_class_name = "nginx"
 	}
-	config.IngressClassName = ingress_class_name
 
-	return &config
+	return &Config{
+		Logger:           logger,
+		K8s:              k8s,
+		Port:             port,
+		IngressClassName: ingress_class_name,
+	}
+}
+
+func getK8sConfig() (*rest.Config, error) {
+	k8sconfig, err := rest.InClusterConfig()
+	if err == nil {
+		return k8sconfig, nil
+	}
+
+	home := os.Getenv("HOME")
+	if home == "" {
+		return nil, fmt.Errorf("Couldn't determine HOME directory, is $HOME set?")
+	}
+	kubeconfig := filepath.Join(home, ".kube", "config")
+	k8sconfig, err = clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to get kubernetes config, both in-cluster and from ~/.kube/config: %s", err)
+	}
+
+	return k8sconfig, nil
 }
