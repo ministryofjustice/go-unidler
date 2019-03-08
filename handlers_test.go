b189b18fb937a132babf16a8e07b2cee2fff0317
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2019-03-08 11:52:23 +0000

    Added test for index handler
    This handler simply responds with the HTML page showing the unidling
    progress.
    
    The test checks that the body is what's expected: Rendered body with
    correct body.

--- handlers_test.go
+++ handlers_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -20,4 +21,28 @@ func TestHealthCheckHandler(t *testing.T) {
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	assert.Equal(t, "Still OK", string(body))
+}
+
+func TestIndexHandler(t *testing.T) {
+	const HOST = "test-tool.example.com"
+
+	ts := httptest.NewServer(http.HandlerFunc(indexHandler))
+	defer ts.Close()
+
+	req, _ := http.NewRequest("GET", ts.URL, nil)
+	req.Host = HOST
+
+	resp, err := http.DefaultClient.Do(req)
+
+	assert.Nil(t, err)
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	// Render index template string
+	var expectedBody bytes.Buffer
+	err = indexTemplates.ExecuteTemplate(&expectedBody, "layout", HOST)
+	assert.Nil(t, err)
+
+	assert.Equal(t, expectedBody.String(), string(body), "Response body didn't match template: '%s'", expectedBody.String())
 }
